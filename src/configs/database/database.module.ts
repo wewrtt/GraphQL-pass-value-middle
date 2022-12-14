010134689922cf833eab8f4d9326282fcb9f6bf9
Trịnh Thành Long <longtt1@vmodev.com>
2022-12-14 15:32:00 +0700

    feature : first commit

--- src/configs/database/database.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { mysqlProviders } from './typeorm.provider';
-
-@Module({
-  imports: [],
-  providers: [...mysqlProviders],
-  exports: [...mysqlProviders],
-})
-export class DatabaseModule { }
--- src/modules/category/category.constant.ts
+++
@@ -1,4 +0,0 @@
-export const ENTITY_CONST = {
-  MODEL_NAME: 'category',
-  MODEL_PROVIDER: 'CATEGORY_MODEL',
-};
